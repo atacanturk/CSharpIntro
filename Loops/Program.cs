33549ddbb8419893540b652e5499aed055f99842
Ata Can Türk <atacanturk97@gmail.com>
2021-01-26 03:04:28 +0300

    Loops added.

---
+++ Loops/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string course1 = "Mathematic";
+            string course2 = "Physics";
+            string course3 = "Chemistry";
+            string course4 = "Biology";
+
+            //array
+
+            string[] courses = new string[] { course1, course2, course3, course4 };
+
+            for (int i = 0; i < courses.Length; i++)
+            {
+                Console.WriteLine(courses[i]);
+            }
+
+            Console.WriteLine("---------");
+
+            foreach (var course in courses)
+            {
+                Console.WriteLine(course);
+            }
+        }
+    }
+}
