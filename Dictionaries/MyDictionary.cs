f8c9e56f204099e605ab1fe3928452d9e77416f8
Ata Can Türk <atacanturk97@gmail.com>
2021-01-28 19:04:45 +0300

    Dictionaries added.

---
+++ Dictionaries/MyDictionary.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionaries
+{
+    class MyDictionary<TKey,TValue>
+    {
+        TKey[] keys;
+        TValue[] values;
+
+        TKey[] tempKeys;
+        TValue[] tempValues;
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+        public void Add(TKey key, TValue value)
+        {
+            tempKeys = keys;
+            tempValues = values;
+
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+                values[i] = tempValues[i];
+                
+
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+
+
+        }
+
+        public TKey[] writeKey
+        {
+            get { return keys; }
+        }
+
+        public TValue[] writeValue
+        {
+            get { return values; }
+        }
+            
+
+    }
+}
---
+++ Dictionaries/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Dictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "Deniz");
+            myDictionary.Add(2, "Ata");
+
+            Console.WriteLine(myDictionary.writeKey[0]); //myDictionary'de birinci elemanın key'ini verir
+            Console.WriteLine(myDictionary.writeValue[0]);
+
+            Console.WriteLine("============");
+
+            Console.WriteLine(myDictionary.writeKey[1]);//myDictionary'de ikinci elemanın key'ini verir
+            Console.WriteLine(myDictionary.writeValue[1]);
+
+        }
+    }
+}
